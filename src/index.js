6b9528627a6e7f33e73cf74448a7396f0ccf8d9d
Justin <justinzhao893@gmail.com>
2025-07-21 11:18:44 +1000

    index.js updated.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client'; // Correct import for React 18+
+import './index.css'; // Assuming you have an index.css file for global styles
+import App from './App'; // Import your App component
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Find the root DOM element where your React app will be mounted
+const container = document.getElementById('root');
+
+// Create a root for the React application
+const root = createRoot(container);
+
+// Render your App component into the root
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
